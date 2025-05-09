907e6247212e59062dfec82a709db619d66310b4
Alexandr <feldman87@yandex.ru>
2025-05-09 13:29:29 +0700

    feat: Реализовать класс InsertionSort

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+import java.util.Arrays;
+import java.util.Random;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        int [] arr = InsertionSort.sort( new int [] {5,3,1,4});
+        System.out.println(Arrays.toString(arr));
+    }
+}
